a1a505bc8ceac59c2e1a7c79f8004033e1441b44
Ribake Ikumoluyi <reiybake05@googlemail.com>
2018-05-18 07:42:03 +0100

    Fixed issue with full stop after answer causing hackerank to fail solution. Fixed last failing test case. Tidied up code.

--- src/main/java/com/thirtytwotwenty/thirtydayjava/day2/Solution.java
+++ src/main/java/com/thirtytwotwenty/thirtydayjava/day2/Solution.java
@@ -1,7 +1,28 @@
 package com.thirtytwotwenty.thirtydayjava.day2;
 
+import java.util.Scanner;
+
 /**
  * Created by ribake on 13/05/2018.
  */
 public class Solution {
+
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        double meal_cost = scanner.nextDouble();
+        int tip_percent = scanner.nextInt();
+        int tax_percent = scanner.nextInt();
+        scanner.close();
+
+        double tip_percentPrecise = (double)tip_percent/100;
+        double tipCost = meal_cost * tip_percentPrecise;
+
+        double tax_percentPrecise = (double)tax_percent/100;
+        double taxCost = meal_cost * tax_percentPrecise;
+
+        double mealTotalPrecise = meal_cost + tipCost + taxCost;
+        long mealTotal = Math.round(mealTotalPrecise);
+
+        System.out.println("The total meal cost is " + mealTotal + " dollars");
+    }
 }
