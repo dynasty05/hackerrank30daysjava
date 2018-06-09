d0895830238006de81aeb84283e133c602923bdb
Ribake Ikumoluyi <reiybake05@googlemail.com>
2018-06-09 09:04:54 +0100

    Completed challenge with own solution. SolutionTest has implementation if Hackerrank's suggestion

--- src/main/java/com/thirtytwotwenty/Solution.java
+++ src/main/java/com/thirtytwotwenty/Solution.java
@@ -1,13 +1,52 @@
 package com.thirtytwotwenty;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class Solution
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String,String> phoneBook = null;
+
+        // number of entries to read.
+        int n = scanner.nextInt();
+        if(n > 0) {
+            phoneBook = new HashMap<>();
+
+            // reached end of line. Move to start of next line
+            scanner.nextLine();
+
+            // Read n lines from input stream
+            while(n > 0) {
+//                String current = scanner.nextLine();
+//                String[] entry = current.split(" ");
+//                phoneBook.put(entry[0],entry[1]);
+
+                phoneBook.put(scanner.next(), scanner.next());
+                n--;
+            }
+        }
+
+        // begin reading the entries to look up
+        String name, phoneNumber = null;
+        while(scanner.hasNext()) {
+            name = scanner.next();
+            if((phoneNumber = phoneBook.get(name)) != null) {
+                System.out.printf("%s=%s", name, phoneNumber);
+                System.out.println("");
+
+            } else {
+                System.out.println("Not found");
+            }
+        }
+
+        // close scanner
+        scanner.close();
+
     }
 }
--- src/test/java/com/thirtytwotwenty/SolutionTest.java
+++ src/test/java/com/thirtytwotwenty/SolutionTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * Unit test for simple Solution.
  */
@@ -16,5 +19,31 @@ public class SolutionTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    // BASED ON HACKERANK'S SOLUTION TEMPLATE
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        HashMap<String,String> phoneBook = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            phoneBook.put(name, new Integer(phone).toString());
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if(phoneBook.get(s) != null) {
+                System.out.printf("%s=%s", s, phoneBook.get(s));
+                System.out.println("");
+
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
     }
 }
