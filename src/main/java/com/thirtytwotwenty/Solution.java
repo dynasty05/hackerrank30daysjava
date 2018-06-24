0b31277e2e7a979851ff1fd8f9143602a4ce4248
Ribake Ikumoluyi <reiybake05@googlemail.com>
2018-06-25 00:49:21 +0100

    Fixed bugs in solution and got all test cases passing

--- src/main/java/com/thirtytwotwenty/Solution.java
+++ src/main/java/com/thirtytwotwenty/Solution.java
@@ -1,6 +1,9 @@
 package com.thirtytwotwenty;
 
 import java.util.Scanner;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Hello world!
@@ -25,6 +28,39 @@ public class Solution
         }
 
         scanner.close();
+
+        SortedSet<Integer> hourglassSums = new TreeSet<Integer>();
+
+        //move through a row n and row n+2 of the 2d array
+        int row = 0;
+        while(row+2 < arr.length) {
+            //move through each column in the 2d array
+            int column = 0;
+
+            while(column+2 < arr.length) {
+                //sum up first three consecutive column elements
+                int columnTracker = column;
+                int trackEnd = columnTracker+3;
+                int hourglassSum = 0;
+
+                while(columnTracker < trackEnd) {
+                    hourglassSum += arr[row][columnTracker] + arr[row+2][columnTracker];
+                    if(columnTracker == trackEnd-2) {
+                        hourglassSum += arr[row+1][columnTracker];
+                    }
+                    columnTracker++;
+                }
+                // add latest hourglass sum to the set of hourglass sums
+                hourglassSums.add(hourglassSum);
+
+                column++;
+            }
+
+            row++;
+        }
+
+     // retrieve the largest value in treeset
+        System.out.println(hourglassSums.last());
 
     }
 }
